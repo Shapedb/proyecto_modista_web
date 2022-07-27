b3b667eb90869fad5328529f4270ece1c95bc7a2
Shapedb <jsilvanmendez@gmail.com>
2022-07-27 10:41:02 -0500

    dsa

--- javascript/nav-bar.js
+++ javascript/nav-bar.js
@@ -1,6 +1,6 @@
 /*Animacion de Barra de Navegacion*/
 const nav = document.querySelector('.nav');
 
-    window.addEventListener ('scroll', function(){
+    window.addEventListener ('scroll', function() {
         nav.classList.toggle('active', window.scrollY >0)
     })
