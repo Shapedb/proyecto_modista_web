0613a785a8402d05cb9fc9ca795441023072c4ee
Shapedb <jsilvanmendez@gmail.com>
2022-08-09 22:31:54 -0500

    dasd

--- javascript/nav-bar.js
+++
@@ -1,6 +0,0 @@
-/*Animacion de Barra de Navegacion*/
-const nav = document.querySelector('.nav');
-
-    window.addEventListener ('scroll', function() {
-        nav.classList.toggle('active', window.scrollY >0)
-    })
