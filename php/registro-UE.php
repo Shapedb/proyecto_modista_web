8b89dd6a47f32d8347baa2c21b0c917f3259515a
Shapedb <jsilvanmendez@gmail.com>
2022-08-22 22:41:35 -0500

    dsd

--- php/registro-UE.php
+++ php/registro-UE.php
@@ -9,50 +9,47 @@ if ($inc) {
     $consulta = "SELECT CVE_PERSONA FROM `personas` ORDER BY CVE_PERSONA DESC LIMIT 1;  ";
     $resultado = mysqli_query($conexion,$consulta);
     $numero = $resultado->fetch_array();
-    $idPersona = $numero['CVE_TIPO_ROPA'] + 1;
+    $idPersona = $numero['CVE_PERSONA'] + 1;
 }
 
-$nomNombre=$_POST[''];
-$nomAPaterno=$_POST[''];
-$nomAMaterno=$_POST[''];
-$nomTel=$_POST[''];
-$nomCElectronico=$_POST[''];
-$nomTipoEm=$_POST[''];
-$nomCurp=$_POST[''];
-$nomUsuario=$_POST[''];
-$nomTiUsu=$_POST[''];
-$nomContraseña=$_POST[''];
+$nomNombre=$_POST['txtNombre'];
+$nomAPaterno=$_POST['txtAPaterno'];
+$nomAMaterno=$_POST['txtAMaterno'];
+$nomTel=$_POST['txtTelefono'];
+$nomCElectronico=$_POST['txtCorreo'];
+$nomTipoEm=$_POST['txtTipoEm'];
+$nomCurp=$_POST['txtCURP'];
+$nomUsuario=$_POST['txtUsuario'];
+$nomTiUsu=$_POST['txtTiUsu'];
+$nomContraseña=$_POST['txtContraseña'];
 
 
 /*Insertar Datos Personales*/
 
 $consulta_1 = "INSERT INTO `personas`(`CVE_PERSONA`, `NOMBRE`, `APELLIDO_PAT`, `APELLIDO_MAT`, `TELEFONO`, `CORREO_ELECTRONICO`) 
-VALUES (' $idPersona','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]')";
+VALUES (' $idPersona','$nomNombre','$nomAPaterno','$nomAMaterno','$nomTel','$nomCElectronico')";
 
 /*Insertar Datos de Empleado*/
 
 $consulta_2 = "INSERT INTO `empleado`(`CVE_EMPLEADO`, `CVE_TIP_EMPLEADO`, `CURP`)
-VALUES (' $idPersona','[value-2]','[value-3]')";
+VALUES (' $idPersona','$nomTipoEm','$nomCurp')";
 
 /*Insertar Datos de Usuario*/
-
 $consulta_3 = "INSERT INTO `usuario`(`CVE_USUARIO`, `USUARIO`, `CVE_TIPO_USUARIO`, `CONTRASENA`) 
-VALUES (' $idPersona','[value-2]','[value-3]','[value-4]'))";
+VALUES ('$idPersona','$nomUsuario','$nomTiUsu','$nomContraseña')";
 
 
 
-$resultado_1=mysqli_query($conexion,$consulta_1);
+$resultado=mysqli_query($conexion,$consulta_1);
 
-$resultado_2=mysqli_query($conexion,$consulta_2);
+mysqli_query($conexion,$consulta_2);
 
-$resultado_3=mysqli_query($conexion,$consulta_3);
+$resultado2 = mysqli_query($conexion,$consulta_3);
 
-
-if ($resultado_1) {
+if ($resultado2) {
     echo " <script> alert('Se ha añadido al Usuario con exito.'); 
     window.location='../layout/administarar-usuarios.php' </script>";
 }
-
 
 mysqli_close($conexion);
 ?>
