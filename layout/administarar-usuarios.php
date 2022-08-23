76792078d91890e5ddd0c67f323c8ed9d92463c1
Shapedb <jsilvanmendez@gmail.com>
2022-08-22 21:34:45 -0500

    dsdsd

--- layout/administarar-usuarios.php
+++ layout/administarar-usuarios.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Ropa | Modista Rochy </title>
+    <title> Administrar Usuario | Modista Rochy </title>
     <!--Boostrap-->
         <!--CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
@@ -13,32 +13,29 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script> 
 
 </head>
-<body id="body">
+<body id="body" class="body">
 
         <?php include('sidebars.php') ?>
 
-        <form action="../php/registro-ropa.php" method="POST">
-            <div class="input-group input-group-sm mb-3">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Agregar Ropa</span>
-                <input placeholder="Nueva Ropa" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" name="txtRopa">
-                <button type="submit" class="btn btn-primary">Agregar</button>
-            </div> 
-        </form>
+        <h1>Usuarios</h1>
 
         <hr>
         <div class="d-flex justify-content-center">
-        <table class="table w-75">
+        <table class="table w-100">
             <thead>
                 <tr>
-                    <th scope="col">Clave</th>
-                    <th scope="col">Tipo de Ropa</th>
-                    <th scope="col">Acciones</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellido Paterno</th>
+                    <th scope="col">Apellido Materno</th>
+                    <th scope="col">Usuario</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col">Contraseña</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
                     $inc = include('../php/database/conexion.php');
-                    $sql = "SELECT * FROM `tipo_ropa`";
+                    $sql = "SELECT p.NOMBRE, p.APELLIDO_PAT,p.APELLIDO_MAT,u.USUARIO,tu.TIPO ,u.CONTRASENA FROM `usuario` as u, `tipo_usuario` as tu, `personas` as p WHERE P.CVE_PERSONA = U.CVE_USUARIO and u.CVE_TIPO_USUARIO = tu.CVE_TIPO_USUARIO";
                     $resul = mysqli_query($conexion,$sql);
 
                     while ($mostrar = mysqli_fetch_array($resul)) {
@@ -46,12 +43,12 @@
                    
                 ?>
                 <tr>
-                    <td><?php echo $mostrar['CVE_TIPO_ROPA'] ?></td>
                     <td><?php echo $mostrar['NOMBRE'] ?></td>
-                    <td>
-                        <input class="btn btn-outline-successy" type="submit" value="Editar">
-                        <input class="btn btn-outline-danger" type="reset" value="Eliminar">
-                    </td>
+                    <td><?php echo $mostrar['APELLIDO_PAT'] ?></td>
+                    <td><?php echo $mostrar['APELLIDO_MAT'] ?></td>
+                    <td><?php echo $mostrar['USUARIO'] ?></td>
+                    <td><?php echo $mostrar['TIPO'] ?></td>
+                    <td><?php echo $mostrar['CONTRASENA'] ?></td>
                 </tr>
                 <?php
                     }
