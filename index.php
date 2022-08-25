3d6d7b446fa5d63fd18d1b6c20370542764d9159
Shapedb <jsilvanmendez@gmail.com>
2022-08-24 22:09:14 -0500

    dsd

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 					</div>
 					<div class="card fat">
 						<div class="card-body">
-							<h4 class="card-title">Login</h4>
+							<h4 class="card-title">Iniciar Sesion</h4>
 							<form method="POST" class="my-login-validation" novalidate="">
 								<div class="form-group">
 									<label for="email">Correo Electronico</label>
