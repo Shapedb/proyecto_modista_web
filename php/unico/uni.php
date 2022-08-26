9cb8b4f4c2708f72bbc397761c93df2341795595
Shapedb <jsilvanmendez@gmail.com>
2022-08-25 19:36:02 -0500

    dasddads

--- php/unico/uni.php
+++ php/unico/uni.php
@@ -2,18 +2,15 @@
 
 include('../database/conexion.php');
 
-$sql1= "SELECT tp.NOMBRE as N, m.MEDIDA as M FROM `tipo_medida` as tm, `tipo_ropa` as tp, `medidas` as m 
-WHERE tm.CVE_MEDIDA = m.CVE_MEDIDA AND tm.CVE_TIPO_ROPA = tp.CVE_TIPO_ROPA AND tm.CVE_TIPO_ROPA  = 7 ";
 
-$consul1 = mysqli_query($conexion,$sql1);
+$txt = $_POST['txt'];
 
 
+  for ($i=0;$i<count($txt);$i++)
+  {
+    print ($txt[$i]);
 
-
-/*$datos = [ "id"=>  ];   */
-$row = mysqli_num_rows($consul1);
-
-echo $row;
+  }
 
 
 
